567d99e1d8cd82649718c99cfb9d813fb4412168
Razichennouf <razichennouf@live.fr>
2023-05-19 22:51:55 +0000

    ssl

--- dsvpwa/__init__.py
+++ dsvpwa/__init__.py
@@ -1,7 +1,7 @@
 
-__project__ = 'DSVPWA'
+__project__ = 'vulrenable_application'
 __description__ = 'Damn Simple Vulnerable Python Web Application (DSVPWA)'
-__author__ = 'Gabor Seljan'
+__author__ = 'Razi chennouf'
 __version__ = '0.0.1'
-__url__ = 'https://github.com/sgabe/DSVPWA'
+__url__ = 'https://github.com/Razichennouf/vulrenable_application'
 __date__ = '2021/09/21'
