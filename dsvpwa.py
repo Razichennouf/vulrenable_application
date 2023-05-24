0e4e17b262b7cbb2de59e26cdc1d8eb26a699098
Razichennouf <razichennouf@live.fr>
2023-05-24 12:22:30 +0000

    latest

--- dsvpwa.py
+++ dsvpwa.py
@@ -10,21 +10,20 @@ from dsvpwa.handlers import VulnHTTPRequestHandler
 BUILD_VER = os.getenv('BUILD_VER') or '0.0.1'
 BUILD_REV = os.getenv('BUILD_REV') or 'dev'
 
-
 def main():
-    parser = argparse.ArgumentParser(
-        prog='DSVPWA',
-        description='Damn Simple Vulnerable Python Web Application'
-    )
-    parser.add_argument('--host', default='0.0.0.0', help='set the IP address to bind to (defaults to 0.0.0.0)')
-    parser.add_argument('--port', type=int, default=os.getenv('DSVPWA_PORT', 443),
-                        help='set the port number to bind to (defaults to 443)')
-    parser.add_argument('--risk', type=int, default=os.getenv('DSVPWA_RISK', 1), choices=range(1, 4),
-                        help='set the risk level in the range 1-3')
-    parser.add_argument('--ssl', action='store_true', default=os.getenv('DSVPWA_SSL', False),
-                        help='enable encryption (defaults to false)')
+
+    parser = argparse.ArgumentParser(prog='DSVPWA',
+        description='Damn Simple Vulnerable Python Web Application')
+    parser.add_argument('--host', default='0.0.0.0',
+        help='set the IP address to bind to (defaults to 0.0.0.0)')
+    parser.add_argument('--port', type=int, default=os.getenv('DSVPWA_PORT', 80),
+        help='set the port number to bind to (defaults to 80)')
+    parser.add_argument('--risk', type=int, default=os.getenv('DSVPWA_RISK', 1), choices=range(1,4),
+        help='set the risk level in the range 1-3')
+    parser.add_argument('--ssl', action='store_true', default=os.getenv('DSVPWA_SSL', 0),
+        help='enable encryption (defaults to false)')
     parser.add_argument('--version', action='version',
-                        version='%(prog)s v{} ({})'.format(BUILD_VER, BUILD_REV))
+        version='%(prog)s v{} ({})'.format(BUILD_VER, BUILD_REV))
 
     args = parser.parse_args()
     proto = 'http' if not args.ssl else 'https'
@@ -34,18 +33,15 @@ def main():
         httpd.RequestHandlerClass.risk = args.risk
 
         if args.ssl:
-            certfile = 'ssl/cert.pem'  # Replace with the path to your SSL certificate file
-            keyfile = 'ssl/key.pem'  # Replace with the path to your SSL private key file
-
-            ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-            #ctx.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1  # Optional: Disable older TLS versions if desired
-            ctx.load_cert_chain(certfile=certfile, keyfile=keyfile)
-
+            ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            ctx.options &= ~ssl.OP_NO_SSLv3
+            ctx.options &= ~ssl.OP_NO_COMPRESSION
+            ctx.options &= ~ssl.OP_CIPHER_SERVER_PREFERENCE
+            ctx.load_cert_chain(certfile='./ssl/cert.pem', keyfile='./ssl/key.pem')
             httpd.socket = ctx.wrap_socket(httpd.socket, server_side=True)
 
         print('[*] Navigate to {}://{}:{} to access DSVPWA'.format(proto, args.host, args.port))
         httpd.serve_forever()
-
     except KeyboardInterrupt:
         print('[*] Quitting...')
         pass
---
+++ gunicorn_config.py
@@ -0,0 +1,2 @@
+bind = '0.0.0.0:8000'  # Set the host and port for Gunicorn
+workers = 2  # Adjust the number of worker processes based on your requirements
